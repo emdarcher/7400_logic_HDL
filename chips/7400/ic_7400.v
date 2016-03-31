0756a72473a7e90c7a269249f7af59f1281f0f89
Ethan Durrant <emd519@yahoo.com>
2016-03-31 01:19:17 -0600

    made the first chip, 7400

---
+++ chips/7400/ic_7400.v
@@ -0,0 +1,16 @@
+
+//7400 Quad 2-input NAND gate
+module ic_7400(
+    Y1, A1, B1,
+    Y2, A2, B2,
+    Y3, A3, B3,
+    Y4, A4, B4);
+    output Y1, Y2, Y3, Y4;
+    input A1, B1, A2, B2, A3, B3, A4, B4;
+
+    assign Y1 = ~(A1 & B1); 
+    assign Y2 = ~(A2 & B2); 
+    assign Y3 = ~(A3 & B3); 
+    assign Y4 = ~(A4 & B4); 
+
+endmodule
